a420c2866367925e2e215cbe45d9712278925171
unknown <qwerty2244668800@gmail.com>
2020-06-27 13:57:00 +0200

    initial

---
+++ client/src/hooks/http.hook.js
@@ -0,0 +1,34 @@
+import {useState, useCallback} from 'react'
+import { set } from 'mongoose';
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+        setLoading(true)
+        try {
+            if(body) {
+                body = JSON.stringify(body)
+                headers['Content-Type'] = 'application/json'
+            }
+            const response = await fetch(url, { method, body, headers });
+            const data = await response.json();
+
+            if(!response.ok) {
+                throw new Error(data.message || 'Что то пошло не так')
+            }
+
+            setLoading(false)
+
+            return data 
+        } catch (e) {
+            setLoading(false)
+            setError(e.message)
+        }
+    }, [])
+
+    const clearError = useCallback(() => setError(null), [])
+
+    return {loading, request, error, clearError}
+}
